5270c3f4d4fdba1246600427457b386a3eaf2b7a
Robin <Robin.Hoogendijk@hva.nl>
2023-09-28 21:18:44 +0200

    Week 1: classes aangevuld

--- src/practicumopdracht/models/Chapter.java
+++ src/practicumopdracht/models/Chapter.java
@@ -1,4 +1,19 @@
 package practicumopdracht.models;
 
+import java.time.LocalDate;
+
 public class Chapter {
+    private Comic belongsTo;
+    private String title;
+    private int chapterNumber;
+    private LocalDate releaseDate;
+    private boolean isLiked;
+
+    public Chapter(Comic belongsTo, String title, int chapterNumber, LocalDate releaseDate, boolean isLiked) {
+        this.belongsTo = belongsTo;
+        this.title = title;
+        this.chapterNumber = chapterNumber;
+        this.releaseDate = releaseDate;
+        this.isLiked = isLiked;
+    }
 }
--- src/practicumopdracht/models/Comic.java
+++ src/practicumopdracht/models/Comic.java
@@ -1,4 +1,15 @@
 package practicumopdracht.models;
 
 public class Comic {
+    private String name;
+    private double rating;
+    private String author;
+    private String description;
+
+    public Comic(String name, double rating, String author, String description) {
+        this.name = name;
+        this.rating = rating;
+        this.author = author;
+        this.description = description;
+    }
 }
